0b844f6549c5ec53792a5d6a3065ad1d45e141c7
Grace-254 <gracekavutu6@gmail.com>
2025-11-22 14:43:52 +0300

    Add perform_operation function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,16 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation =="add":
+        return(num1 + num2)
+    if operation =="subtraction":
+        return (num1 - num2)
+    if operation =="multiplication":
+        return (num1 * num2)
+    if operation =="division":
+        if num2 == 0:
+            return "you cannot divide by zero"
+        else:
+            return(num1 / num2)
+
+    
+    
 
